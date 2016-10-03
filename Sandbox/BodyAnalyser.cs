138b3923f0bd3cf4a75d721fa7906331b32a59fa
Martin Kierkegaard <mark@easj.dk>
2016-10-03 21:48:24 +0200

    Add project files.

---
+++ Sandbox/BodyAnalyser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class BodyAnalyser
+    {
+        // Given the weight and height of a person, return the BMI
+        public double CalculateBMI(double heightInMeters, double weightInKilo)
+        {
+            double bmi = weightInKilo / (heightInMeters * heightInMeters);
+            return bmi;
+        }
+
+        // Given the weight and height of a person, return a brief analysis
+        // of the BMI of that person
+        public String AnalyseBMI(double heightInMeters, double weightInKilo)
+        {
+            // The below code must be changed
+            String analysis = "All people are beautiful, YOU are beautiful!";
+
+            return analysis;
+        }
+
+    }
+}

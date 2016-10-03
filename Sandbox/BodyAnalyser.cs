83b91a8b2680dec57265b4e69ee0c9865e976ca7
Martin Kierkegaard <mark@easj.dk>
2016-10-03 21:52:39 +0200

    solution for the exercise

--- Sandbox/BodyAnalyser.cs
+++ Sandbox/BodyAnalyser.cs
@@ -18,11 +18,31 @@ namespace Sandbox
         // of the BMI of that person
         public String AnalyseBMI(double heightInMeters, double weightInKilo)
         {
-            // The below code must be changed
-            String analysis = "All people are beautiful, YOU are beautiful!";
+            double bmi = CalculateBMI(heightInMeters, weightInKilo);
+            string analysis = "Your BMI is : " + bmi + ", ";
+
+            if (bmi < 15)
+            {
+                analysis = analysis + "you are way too skinny!";
+            }
+            else if ((bmi >= 15) && (bmi <= 22))
+            {
+                analysis = analysis + "you are a bit skinny";
+            }
+            else if ((bmi > 22) && (bmi <= 28))
+            {
+                analysis = analysis + "you are just fine!";
+            }
+            else if ((bmi > 28) && (bmi <= 35))
+            {
+                analysis = analysis + "you are a bit overweight!";
+            }
+            else
+            {
+                analysis = analysis + "you are way too fat!";
+            }
 
             return analysis;
         }
-
     }
 }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,6 +8,17 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
+            BodyAnalyser ba = new BodyAnalyser();
+
+            Console.WriteLine(ba.AnalyseBMI(1.60, 35));
+            Console.WriteLine(ba.AnalyseBMI(1.60, 45));
+            Console.WriteLine(ba.AnalyseBMI(1.60, 55));
+            Console.WriteLine(ba.AnalyseBMI(1.60, 65));
+            Console.WriteLine(ba.AnalyseBMI(1.60, 75));
+            Console.WriteLine(ba.AnalyseBMI(1.60, 85));
+            Console.WriteLine(ba.AnalyseBMI(1.60, 95));
+            Console.WriteLine(ba.AnalyseBMI(1.60, 105));
+
             // The LAST line of code should be ABOVE this line
         }
     }

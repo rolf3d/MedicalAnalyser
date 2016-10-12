6a52089729774f58407a033e9dbe23a8e580d2bf
Rolf Petersen <rollep7@msn.com>
2016-10-12 16:02:10 +0200

    AnalyseBMI udbygget metode  og udskriv til console.

--- Sandbox/BodyAnalyser.cs
+++ Sandbox/BodyAnalyser.cs
@@ -7,19 +7,47 @@ namespace Sandbox
 {
     class BodyAnalyser
     {
+        
+         
+
         // Given the weight and height of a person, return the BMI
-        public double CalculateBMI(double heightInMeters, double weightInKilo)
-        {
-            double bmi = weightInKilo / (heightInMeters * heightInMeters);
-            return bmi;
-        }
+        //public double CalculateBMI(double heightInMeters, double weightInKilo)
+        //{
+        //    double bmi = weightInKilo / (heightInMeters * heightInMeters);
+        //    return bmi;
+        //}
 
         // Given the weight and height of a person, return a brief analysis
         // of the BMI of that person
         public String AnalyseBMI(double heightInMeters, double weightInKilo)
         {
+            
+            double bmi = weightInKilo / (heightInMeters * heightInMeters);
+            
+            String analysis = "";
             // The below code must be changed
-            String analysis = "All people are beautiful, YOU are beautiful!";
+            if (bmi < 15)
+            {
+                analysis = "You are way too skinny!";
+            }
+            else if (bmi > 15 && bmi < 22)
+            {
+                analysis = "You are a bit skinny!";
+            }
+            else if (bmi > 22 && bmi < 28)
+            {
+                analysis = "You are just fine!";
+            }
+            else if (bmi > 28 && bmi < 35)
+            {
+                analysis = "You ar ea bit overweight!";
+            }
+            else
+            {
+                analysis = "You are way too fat!";
+            }
+
+            //String analysis = "All people are beautiful, YOU are beautiful!";
 
             return analysis;
         }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -7,7 +7,9 @@ namespace Sandbox
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
-
+            BodyAnalyser minbodyanalyse = new BodyAnalyser();
+            //minbodyanalyse.CalculateBMI(1.8, 88);
+            Console.WriteLine("{0}",minbodyanalyse.AnalyseBMI(1.8,88));
             // The LAST line of code should be ABOVE this line
         }
     }
